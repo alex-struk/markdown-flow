f170a3a06ec2b186a1df45b4a0f37408f41a3ea7
Struk <Alex.Struk@gov.bc.ca>
2022-11-17 10:05:35 -0800

    cors fix

--- apiServer/server.js
+++ apiServer/server.js
@@ -3,9 +3,16 @@ const cors = require("cors");
 
 const app = express();
 
+var whitelist = ["http://localhost:3000", 'https://digital-gov-frontend-pr-1119-c0cce6-dev.apps.silver.devops.gov.bc.ca']
 var corsOptions = {
-  origin: "http://localhost:3000"
-};
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.use(cors(corsOptions));
 
